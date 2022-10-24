3bfca0167fd7f105f6d7d13a1629803a0735cf5b
Eduardo <105960444+Eduard0803@users.noreply.github.com>
2022-10-24 10:53:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ webcam = cv2.VideoCapture(0)    #Faz a Captura da foto
 
 while (True):
     validacao, frame = webcam.read()
-    cv2.imshow("img1", frame)
     break
-cv2.imwrite("Foto_teste.png", frame)    #Salva a foto no arquivo "Foto_teste.png"
+cv2.imwrite("Foto_teste(1).png", frame)    #Salva a foto no arquivo "Foto_teste(1).png"
 cv2.destroyAllWindows()
 
 webcam.release()
