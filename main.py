aeb725eb3b50f538f881774f1323b8046988471b
Eduardo <105960444+Eduard0803@users.noreply.github.com>
2022-10-24 10:46:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import cv2
+import cv2      #Biblioteca usada para controlar a WebCam
 
-webcam = cv2.VideoCapture(0)
+webcam = cv2.VideoCapture(0)    #Faz a Captura da foto
 
 while (True):
     validacao, frame = webcam.read()
     cv2.imshow("img1", frame)
     break
-cv2.imwrite("Foto_teste.png", frame)
+cv2.imwrite("Foto_teste.png", frame)    #Salva a foto no arquivo "Foto_teste.png"
 cv2.destroyAllWindows()
 
 webcam.release()
